410554344431d9d10e9cad71833ee8de739e2caa
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-04-17 23:15:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ const passportJWT=require('./Config/Passport')
 
 app.use("/api",router)
 
-app.listen(PORT,()=>console.log("Server running on port "+PORT))
+app.listen(process.env.PORT ||PORT,()=>console.log("Server running on port "+PORT))
