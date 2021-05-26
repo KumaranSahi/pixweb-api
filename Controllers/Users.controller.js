04976a13c8ccf58c7933cbf6d500e3b445dced1a
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-05-26 20:57:36 +0530

    refactor-User controller refactor

--- Controllers/Users.controller.js
+++ Controllers/Users.controller.js
@@ -1,91 +1,102 @@
-const usersdb=require('../Models/users.model');
-const jwt=require('jsonwebtoken');
+const usersdb = require("../Models/users.model");
+const jwt = require("jsonwebtoken");
+const { emailIdCheck, hashingPasswords,confirmPasswordCheck } = require("../Utils/userUtils");
 
-module.exports.signupUser=async (req,res)=>{
-    const {name,email,password}=req.body;
-    let data=null;
-    try{
-    if(await usersdb.findOne({email:email})){
-        return res.status(208).json({
-            ok:false,
-            message:"User Already exists in the system"
-        })
+module.exports.signupUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  let data = null;
+  if (!name && !email && !password && !emailIdCheck(email)) {
+    return res.status(400).json({
+      ok: false,
+      message: "Invalid Request",
+    });
+  }
+  try {
+    if (await usersdb.findOne({ email: email })) {
+      return res.status(409).json({
+        ok: false,
+        message: "User Already exists in the system",
+      });
     }
-    if(name && (email && new RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$").test(email)) && password){
-        data=await usersdb.create({
-            name:name,
-            email:email,
-            password:password
-        })
+    const newPassword = await hashingPasswords(password, salt);
+    data = await usersdb.create({
+      name: name,
+      email: email,
+      password: newPassword,
+    });
+    if (data) {
+      return res.status(201).json({
+        ok: true,
+        message: "User Added Successfully",
+      });
     }
-    else{
-        return res.status(400).json({
-            ok:false,
-            message:"Invalid Request"
-        })
-    }
-    if(data){
-        return res.status(201).json({
-            ok:true,
-            message:"User Added Successfully"
-        })
-    }    
-    }catch(error){
-        console.log(error)
-        return res.status(503).json({
-            ok:false,
-            message:"Internal error please try again later"
-        })
-    }
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(503).json({
+      ok: false,
+      message: "Internal error please try again later",
+    });
+  }
+};
 
-module.exports.changePassword=async (req,res)=>{
-    const {email,password,confirmPassword}=req.body;
-    if(!(password)||!(confirmPassword)||(password!==confirmPassword)){
-        return res.status(405).json({
-            ok:false,
-            message:"Passwords are invalid"
-        })
+module.exports.changePassword = async (req, res) => {
+  const { email, password, confirmPassword } = req.body;
+  try {
+    if (confirmPasswordCheck(password,confirmPasswordCheck)) {
+      return res.status(405).json({
+        ok: false,
+        message: "Passwords are invalid",
+      });
     }
-    const user=await usersdb.findOne({email:email});
-    if(user){
-        await user.update({password:password});
-        return res.status(200).json({
-            ok:true,
-            message:"Password Updated Successfully"
-        })
-    }else{
-        return res.status(404).json({
-            ok:false,
-            message:"invalid Email ID"
-        })
+    const user = await usersdb.findOne({ email: email });
+    if (user) {
+      const newPassword = await hashingPasswords(password, salt);
+      await user.update({ password: newPassword });
+      return res.status(200).json({
+        ok: true,
+        message: "Password Updated Successfully",
+      });
+    } else {
+      return res.status(404).json({
+        ok: false,
+        message: "invalid Email ID",
+      });
     }
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(503).json({
+      ok: false,
+      message: "Internal error please try again later",
+    });
+  }
+};
 
-module.exports.signinUser=async (req,res)=>{
-    const {email,password}=req.body;
-    try{
-        const user=await usersdb.findOne({email:email})
-        if (!user || user.password !== password){
-            return res.status(401).json({
-                ok:false,
-                message: "Invalid username or password",
-            });
-        }
-        return res.status(200).json({
-            message: 'Sign in successful, here is your token, please keep it safe!',
-            data:  {
-                ok:true,
-                token: jwt.sign(user.toJSON(),process.env["SECRET"], {expiresIn:  '60m'}),
-                userId:user.id,
-                userName:user.name
-            }
-        })
-    }catch(error){
-        console.log(error)
-        return res.status(503).json({
-            ok:false,
-            message:"Internal server error"
-        })
+module.exports.signinUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await usersdb.findOne({ email: email });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).json({
+        ok: false,
+        message: "Invalid username or password",
+      });
     }
-}
+    return res.status(200).json({
+      message: "Sign in successful, here is your token, please keep it safe!",
+      data: {
+        ok: true,
+        token: jwt.sign({ userId: user._id }, process.env["SECRET"], {
+          expiresIn: "24h",
+        }),
+        userId: user.id,
+        userName: user.name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(503).json({
+      ok: false,
+      message: "Internal server error",
+    });
+  }
+};
--- Models/users.model.js
+++ Models/users.model.js
@@ -1,30 +1,47 @@
-const {Schema,model}=require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const userSchema=new Schema({
-    name:{
-        type:String,
-        required:true
-    },email:{
-        type:String,
-        required:true,
-        unique:true
-    },password:{
-        type:String,
-        required:true
-    },playlists:[{
-        type:Schema.Types.ObjectId,
-        ref:'playlist'
-    }],histories:[{
-        type:Schema.Types.ObjectId,
-        ref:'video'
-    }],likes:[{
-        type:Schema.Types.ObjectId,
-        ref:'like'
-    }],notes:[{
-        type:Schema.Types.ObjectId,
-        ref:'note'
-    }]
-},{timestamps:true})
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    playlists: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "playlist",
+      },
+    ],
+    histories: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "like",
+      },
+    ],
+    notes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "note",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-const user=model("user",userSchema);
-module.exports=user;
+const user = model("user", userSchema);
+module.exports = user;
